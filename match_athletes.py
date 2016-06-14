3065c77fc2d6568201790419b2bc3f801701be26
cbishup <colby@setek.io>
2016-06-13 19:30:44 -0600

    Functional Code
    This is the first such of the functioning code. Produces 1300 files.
    Desired < 1000. Typos in coach submission cause duplicate schools.

--- match_athletes.py
+++ match_athletes.py
@@ -1,14 +1,17 @@
-import sys
 import csv
 
 
 def read_file(file):
-	newList = []
-	lines = open(file)
-	for line in lines:
-		temp1 = [x for x in line.split(',')]
-		newList.append(temp1)
-	return newList
+    lines = open(file)
+    newList = []
+    for line in lines:
+        newerList = []
+        temp1 = [x for x in line.split('\r')]
+        for linex in temp1:
+            temp = [y for y in line.split(",")]
+            newerList.append(temp)
+        newList.append(newerList)
+    return newList
 
 def pull_highschool(sorted_file, index):
     for line in range(index,len(sorted_file)):
@@ -22,45 +25,47 @@ def pull_highschool(sorted_file, index):
             else:
                 print newList
 
-def sortcsvbymanyfields(csvfilename, themanyfieldscolumnnumbers):
-	with open(csvfilename, 'rU') as f:
-		readit = csv.reader(f)
-		thedata = list(readit)
-		thedata.sort(key=operator.itemgetter(*themanyfieldscolumnnumbers))
-	with open("new_file", 'wb') as k:
-		writeit = csv.writer(k)
-		writeit.writerows(thedata)
+#def check_highschool(mid,end,spvar):
+	
+
 
 def main():
-	sortcsvbymanyfields("NXR TEST DATA.xlsx - Roster upload TEST DATA.csv",15)
-
-
-	
-	"""reader = csv.reader(open("NXR TEST DATA.xlsx - Roster upload TEST DATA.csv"), delimiter=",")
-	
-	for id, path, title, date, author, platfo, type, port, dog, school, cat, pig, goat, orrr, annnn in reader:
-	    print school"""
-	"""for num in range(958):
-		highschool = base_file[0][9]
+	with open("NXR TEST DATA.xlsx - Roster upload TEST DATA.csv") as csvfile:
+		data = [row for row in csv.reader(csvfile.read().splitlines())]
+		highschool = data[0][9]
+		print len(data)
+		#for num in range(958):
 		newList = []
-		n+=1
-		print n 
-		for line in range(len(base_file)-1):
-			highschool = base_file[line][9]
-			if highschool == base_file[line+1][9]:
-				newList.append(base_file[line])
+		#mid_ind = 0
+		line = 0
+		while line < len(data):
+		#for line in range(mid_ind,len(data)):
+			if (highschool.lower() or highschool.lower() + " ") == data[line][9].lower():
+				newList.append(data[line-1])
+				print "newList"
+				highschool = data[line][9]
+				line+=1
 			else:
-				print line
-				tmp_file = open("%s" % (highschool + " 2016"), "w")
-				for x in newList:
-					tmp_file.write("%s \n" % x)
-				tmp_file.close()"""
-				
-	"""for i in range(958):
-		d = {basefile[i]:basefile[i][9]}
-		print d"""
+
+                    #tmp_file = open("%s" % (highschool + " 2016"), "w")
+                    #for x in newList:
+                        #tmp_file.write("%s \n" % x)
+                       #newList = []
+				newList.append(data[line-1])
+				with open("%s" % (highschool + " 2016"), "w") as fp:
+					a = csv.writer(fp, delimiter=',')
+					a.writerows(newList)
+				newList = []
+				highschool = data[line][9]
+				line+=1
 			
-  
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
